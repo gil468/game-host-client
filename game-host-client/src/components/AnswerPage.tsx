c78e6937d7f26859dce84562865c235f999fa6e9
Oren <oren.hta@gmail.com>
2024-07-07 23:09:15 +0300

    fix

--- game-host-client/src/components/AnswerPage.tsx
+++ game-host-client/src/components/AnswerPage.tsx
@@ -3,25 +3,45 @@ import { useEffect, useState } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import CountdownExample from "./Countdown";
 import useRequests from "../hooks/useRequests";
+import { HttpStatusCode } from "axios";
 
 export interface AnswerPageProps {
-    songName : string
+  songName: string;
 }
 
 const AnswerPage = () => {
-    const [showCountdown, setShowCountdown] = useState<boolean>(false); 
-    const { nextSong } = useRequests();
-    const songName = useLocation().state.songName;
+  const [showCountdown, setShowCountdown] = useState<boolean>(false);
+  const { nextSong, endGame } = useRequests();
+  const { songName, isLastSong } = useLocation().state;
+  const navigate = useNavigate();
 
-    return (
-        <Stack width="95%" alignItems={"center"} spacing={10}>
-        <Typography variant="h3">{`The song is ${songName}`}</Typography>
-        <Button variant="contained" size="large" onClick={() => setShowCountdown(true)}>
+  return (
+    <Stack width="95%" alignItems={"center"} spacing={10}>
+      <Typography variant="h3">{`The song is ${songName}`}</Typography>
+      {isLastSong ? (
+        <Button
+          variant="contained"
+          size="large"
+          onClick={async () => {
+            const res = await endGame();
+            res.status === HttpStatusCode.Ok &&
+              navigate("/end-game", { state: { gameWinner: res.data } });
+          }}
+        >
+          End Game
+        </Button>
+      ) : (
+        <Button
+          variant="contained"
+          size="large"
+          onClick={() => setShowCountdown(true)}
+        >
           Next Song
         </Button>
-        {showCountdown && <CountdownExample onEnd={nextSong}/>}
-      </Stack>
-    )
-}
+      )}
+      {showCountdown && <CountdownExample onEnd={nextSong} />}
+    </Stack>
+  );
+};
 
 export default AnswerPage;
--- game-host-client/src/router/MusicMasterRouter.tsx
+++ game-host-client/src/router/MusicMasterRouter.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
 import GameInProgress from "../components/GameInProgress";
 import MainPage from "../components/MainPage";
 import AnswerPage from "../components/AnswerPage";
@@ -8,54 +13,71 @@ import { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 
 const MusicMasterRouter = () => {
-    const [isPlaying, setIsPlaying] = useState<boolean>(false);
-    const [showCountdown, setShowCountdown] = useState<boolean>(false);
-    const [waitingPlayers, setWaitingPlayers] = useState<string[]>([]);
-    const navigate = useNavigate();
-    useEffect(() => {
-        const socket = io('http://localhost:3000');
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [showCountdown, setShowCountdown] = useState<boolean>(false);
+  const [waitingPlayers, setWaitingPlayers] = useState<string[]>([]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const socket = io("http://localhost:3000");
 
-        socket.on('round-started', (x)=> {
-            navigate('game-in-progress', {state : {songId : x.songId}})
-        })
+    socket.on("round-started", (x) => {
+      navigate("game-in-progress", { state: { songId: x.songId } });
+    });
 
-        socket.on('playerJoined', (player) => {
-          setWaitingPlayers(x => ([...x, player.userName]))
-        })
+    socket.on("playerJoined", (player) => {
+      setWaitingPlayers((x) => [...x, player.userName]);
+    });
 
-        socket.on("buzzerGranted",() => {
-            setIsPlaying(false);
-            socket.on("correctAnswer", (x) => {
-                enqueueSnackbar('correct answer', {variant:'success', autoHideDuration:1000,
-              anchorOrigin : {horizontal : 'center', vertical : 'top'},
-              onClose: () => navigate('/answer-revail', {state : {songName : x}})})
-              socket.off("correctAnswer")
-              socket.off("wrongAnswer")
-            })
-            socket.on("wrongAnswer", (x) => {
-                enqueueSnackbar('wrong answer', {variant:'error', autoHideDuration:1000,
-                anchorOrigin : {horizontal : 'center', vertical : 'top'},
-                onClose: () => setShowCountdown(true)})
-                socket.off("wrongAnswer")
-                socket.off("correctAnswer")
-            })
-        })
+    socket.on("buzzerGranted", () => {
+      setIsPlaying(false);
+      socket.on("correctAnswer", (x) => {
+        enqueueSnackbar("correct answer", {
+          variant: "success",
+          autoHideDuration: 1000,
+          anchorOrigin: { horizontal: "center", vertical: "top" },
+          onClose: () =>
+            navigate("/answer-revail", {
+              state: { songName: x, isLastSong: x === "Wonderwall" },
+            }),
+        });
+        socket.off("correctAnswer");
+        socket.off("wrongAnswer");
+      });
+      socket.on("wrongAnswer", (x) => {
+        enqueueSnackbar("wrong answer", {
+          variant: "error",
+          autoHideDuration: 1000,
+          anchorOrigin: { horizontal: "center", vertical: "top" },
+          onClose: () => setShowCountdown(true),
+        });
+        socket.off("wrongAnswer");
+        socket.off("correctAnswer");
+      });
+    });
 
-        return () => {
-            socket.close();
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  return (
+    <Routes>
+      <Route
+        path="/game-in-progress"
+        element={
+          <GameInProgress
+            isPlaying={isPlaying}
+            setIsPlaying={setIsPlaying}
+            setShowCountdown={setShowCountdown}
+            showCountdown={showCountdown}
+          />
         }
-
-    },[])
-
-    return (
-      <Routes>
-        <Route path="/game-in-progress" element={<GameInProgress isPlaying={isPlaying} setIsPlaying={setIsPlaying}
-         setShowCountdown={setShowCountdown} showCountdown={showCountdown}/>} />
-        <Route path="/" element={<MainPage joinedPlayers={waitingPlayers}/>} />
-        <Route path="/answer-revail" element={<AnswerPage/>} />
-        <Route path="/end-game" element={<EndGamePage/>} />
-      </Routes>
-    )
-}
+      />
+      <Route path="/" element={<MainPage joinedPlayers={waitingPlayers} />} />
+      <Route path="/answer-revail" element={<AnswerPage />} />
+      <Route path="/end-game" element={<EndGamePage />} />
+    </Routes>
+  );
+};
 
 export default MusicMasterRouter;
